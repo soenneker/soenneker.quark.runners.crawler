d5363e6a8ecf644632c83d3100639ba2b95e2936
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:19 -0500

    xml commenting

--- src/Soenneker.Quark.Runners.Crawler/Constants.cs
+++ src/Soenneker.Quark.Runners.Crawler/Constants.cs
@@ -1,11 +1,32 @@
 namespace Soenneker.Quark.Runners.Crawler;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The suite repository.
+    /// </summary>
     public const string SuiteRepository = "https://github.com/soenneker/soenneker.quark.suite.git";
+    /// <summary>
+    /// The crawled repository.
+    /// </summary>
     public const string CrawledRepository = "https://github.com/soenneker/soenneker.quark.crawled.git";
+    /// <summary>
+    /// The components repository.
+    /// </summary>
     public const string ComponentsRepository = "https://github.com/soenneker/soenneker.quark.crawled.components.git";
+    /// <summary>
+    /// The components url.
+    /// </summary>
     public const string ComponentsUrl = "https://soenneker.github.io/soenneker.quark.suite/";
+    /// <summary>
+    /// The crawl base path.
+    /// </summary>
     public const string CrawlBasePath = "soenneker.quark.suite";
+    /// <summary>
+    /// The commit message.
+    /// </summary>
     public const string CommitMessage = "Refresh Quark component preview HTML";
 }
